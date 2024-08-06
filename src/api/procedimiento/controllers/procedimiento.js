138f078aeaca076ee3fb4daa3729e42281ba51ec
Luis Alejandro Muñoz Garcia <luisalejandrm533@gmail.com>
2024-08-06 11:26:39 -0500

    estructura inicial del backend

---
+++ src/api/procedimiento/controllers/procedimiento.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * procedimiento controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::procedimiento.procedimiento');
---
+++ src/api/procedimiento/routes/procedimiento.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * procedimiento router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::procedimiento.procedimiento');
